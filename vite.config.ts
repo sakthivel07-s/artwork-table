228ccc8fe760eae71bbf15694f595c436784a107
Sakthivel Murugan P <144420006+sakthivel07-s@users.noreply.github.com>
2025-07-22 21:11:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/artwork-table/',  // 👈 Very important for GitHub Pages
+  base: '/artwork-table/',  
   plugins: [react()],
 })
